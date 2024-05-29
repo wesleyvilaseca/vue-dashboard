135c487e3664779874d07880a1b3d7f8a66c1ceb
Wesley Vila Seca <wesley.vilaseca@hotmail.com>
2024-05-28 23:55:21 -0300

    feate: global state navmenu

---
+++ src/store/menu/mutations.js
@@ -0,0 +1,5 @@
+export default {
+    SET_SHOW_MENU_MOBILE: (states, value) => {
+        states.showMobileMenu = value;
+    }
+}
---
+++ src/store/menu/state.js
@@ -0,0 +1,80 @@
+export default {
+    menus: [
+        {
+         name: 'Home',
+         link: '/',
+         icon: 'fa-solid fa-house'
+       },
+       {
+         name: 'Mensalidades',
+         link: '/mensalidades',
+         icon: 'fa-solid fa-wallet'
+       },
+       {
+         name: 'Acadêmico',
+         link: '',
+         icon: 'fa-solid fa-school',
+         active: false,
+         submenu: [
+           {
+             name: 'Boletim Acadêmico',
+             link: '/boletim-academico',
+             icon: '',
+           },
+           {
+             name: 'Calendário Acadêmico',
+             link: '/calendario-academico',
+             icon: '',
+           },
+           {
+             name: 'Perguntas Frequentes',
+             link: '/perguntas-frequentes',
+             icon: '',
+           }
+         ]
+       },
+       {
+         name: 'Documentos',
+         link: '',
+         icon: 'fa-regular fa-file',
+         active: false,
+         submenu: [
+           {
+             name: 'Central de documentos',
+             link: '/central-documentos',
+             icon: 'fa-solid fa-file-word',
+           },
+            {
+             name: 'Manual do Aluno',
+             link: '/manual-do-aluno',
+             icon: 'fa-solid fa-bars',
+           },
+           {
+             name: 'Contato',
+             link: '/contato',
+             icon: 'fa-solid fa-headset',
+           }
+         ]
+       },
+       {
+         name: 'Comunicação',
+         link: '',
+         icon: 'fa-solid fa-phone',
+         active: false,
+         submenu: [
+           {
+             name: 'CAD - Central de Atendimento',
+             link: '/cad',
+             icon: '',
+           },
+           {
+             name: 'Comunicados institucionais',
+             link: '/comunicados-institucionais',
+             icon: '',
+           }
+         ]
+       }
+     ],
+     showMobileMenu: false,
+     SIZE_SHOW_MOBILE_MENU: 991,
+}
